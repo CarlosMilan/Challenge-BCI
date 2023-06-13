e1ea63891fff6f415eb171debb0dd06834b0fc3c
Martin Milan <martin.milan3@gmail.com>
2023-06-13 16:56:23 -0300

    Inicio de desafio

---
+++ src/test/java/com/bci/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bci.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
